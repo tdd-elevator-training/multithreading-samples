22c8977e40d7f1b1deef68f6297ef9ecc27a301d
alexander.baglay <apofig@gmil.com>
2015-03-24 18:58:31 +0200

    smples

--- src/main/java/com/apofig/multith/connectionpool/dao/pool/ConnectionRunner.java
+++
@@ -1,8 +0,0 @@
-package com.apofig.multith.connectionpool.dao.pool;
-
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-
-public interface ConnectionRunner<T> {
-    T connect(PreparedStatement statement) throws SQLException;
-}
